d462264e1e60e48c6936977dd51a75d382208748
Ashish-741 <ashishchahar731@gmail.com>
2025-09-27 20:36:41 +0530

    Update 239. Sliding Window Maximum.java

--- Day-01/239. Sliding Window Maximum.java
+++ Day-01/239. Sliding Window Maximum.java
@@ -26,4 +26,40 @@ class Solution {
         }
         return max;
     }
+
+}
+
+//this is the full and optimal sol of this problem using tht priority queue 
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // max-heap storing [value, index]
+        PriorityQueue<int[]> list = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        // first add the first k elements
+        for (int i = 0; i < k; i++) {
+            list.add(new int[]{nums[i], i});
+        }
+        result[0] = list.peek()[0]; // max of first window
+
+        // slide the window
+        for (int i = k; i < n; i++) {
+            // add new element
+            list.add(new int[]{nums[i], i});
+
+            // remove elements outside the current window
+            while (list.peek()[1] <= i - k) {
+                list.poll();
+            }
+
+            // max of current window
+            result[i - k + 1] = list.peek()[0];
+        }
+
+        return result;
+    }
 }
